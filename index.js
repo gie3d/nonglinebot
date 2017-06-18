5c68ba5467eebfe8fa8ca64b6f02e4dd9141a882
Wasa C <gie68i@gmail.com>
2017-06-18 23:28:09 +0700

    New push after auto deploy to heroku has been enabled

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 function handleEvent(event) {
 
     console.log(event);
-
     if (event.type === 'message' && event.message.type === 'text') {
         handleMessageEvent(event);
     } else {
